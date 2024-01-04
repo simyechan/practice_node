739376a5866546af9f9ef08c633ba13ab9ad6870
sim <simyechan87@gmail.com>
2024-01-04 12:21:36 +0900

    홈 페이지 구현

--- main.js
+++ main.js
@@ -5,37 +5,65 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-        title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+    
+    if(pathname === '/') {
+        if(queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var tmplate = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(tmplate);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var title = queryData.id;
+                var tmplate = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(tmplate);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-    var tmplate = `
-    <!doctype html>
-    <html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.end(tmplate);
-    })
     
  
 });
