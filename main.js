6599ab526743eb649d6ab6544dedd99febb01f0d
sim <simyechan87@gmail.com>
2024-01-05 11:48:22 +0900

    삭제 기능 추가

--- main.js
+++ main.js
@@ -51,7 +51,12 @@ var app = http.createServer(function(request,response){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
                     var title = queryData.id;
                     var list = templateList(filelist);
-                    var tmplate = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create<a> <a href="/update?id=${title}">update</a>`);
+                    var tmplate = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create<a> <a href="/update?id=${title}">update</a> 
+                    <form action="delete_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <input type="submit" value="delete">
+                    </form>
+                    `);
                     response.writeHead(200);
                     response.end(tmplate);
                 });
@@ -87,7 +92,7 @@ var app = http.createServer(function(request,response){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
                 response.writeHead(302, {location: `/?id=${title}`});
                 response.end('success');
-            })
+            });
         });
     } else if(pathname === '/update') {
         fs.readdir('./data', function(error, filelist) {
@@ -125,8 +130,21 @@ var app = http.createServer(function(request,response){
                 fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
                     response.writeHead(302, {location: `/?id=${title}`});
                     response.end('success');
-                })
-            })
+                });
+            });
+        });
+    } else if(pathname === '/delete_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(error) {
+                response.writeHead(302, {location: `/`});
+                response.end();
+            });
         });
     } else {
         response.writeHead(404);
