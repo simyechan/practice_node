abfb5418cecf668e69b30d590c94b31277b41649
sim <simyechan87@gmail.com>
2024-01-05 10:47:06 +0900

    글 생성 ui

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create<a>
         ${body}
     </body>
     </html>
@@ -33,10 +34,9 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    
+    console.log(pathname);
     if(pathname === '/') {
         if(queryData.id === undefined) {
-
             fs.readdir('./data', function(error, filelist) {
             var title = 'Welcome';
             var description = 'Hello, Node.js';
@@ -44,8 +44,7 @@ var app = http.createServer(function(request,response){
             var tmplate = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(tmplate);
-            })
-            
+            });
         } else {
             fs.readdir('./data', function(error, filelist) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
@@ -57,7 +56,26 @@ var app = http.createServer(function(request,response){
                 });
             });
         }
-    } else {
+    }else if(pathname === '/create') {
+        fs.readdir('./data', function(error, filelist) {
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var tmplate = templateHTML(title, list, `
+                <form action="http://localhost:3000/process_create" method="post">
+                    <p><input type="text" name=""title placeholder="title"></p>
+                    <p>
+                        <textarea name="description" placeholder="description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                </form>
+            `);
+            response.writeHead(200);
+            response.end(tmplate);
+        });
+    }
+    else {
         response.writeHead(404);
         response.end('Not found');
     }
