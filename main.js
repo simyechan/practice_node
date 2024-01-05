fb40e0c0b89a0601f6d711daa99eccbf668eedff
sim <simyechan87@gmail.com>
2024-01-05 09:30:15 +0900

    글목록 출력

--- main.js
+++ main.js
@@ -9,55 +9,67 @@ var app = http.createServer(function(request,response){
     
     if(pathname === '/') {
         if(queryData.id === undefined) {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+
+            fs.readdir('./data', function(error, filelist) {
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = '<ul>';
+            var i = 0;
+            while (i < filelist.length) {
+                list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                i += 1;
+            }
+            list += '</ul>'
+            var tmplate = `
+            <!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(tmplate);
+            })
+            
+        } else {
+            fs.readdir('./data', function(error, filelist) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
-                var tmplate = `
-                <!doctype html>
-                <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ul>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-                </html>
-                `;
-                response.writeHead(200);
-                response.end(tmplate);
-            });
-        } else {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-                var title = queryData.id;
-                var tmplate = `
-                <!doctype html>
-                <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ul>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-                </html>
-                `;
-                response.writeHead(200);
-                response.end(tmplate);
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i += 1;
+                }
+                list +='</ul>'
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                    var title = queryData.id;
+                    var tmplate = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(tmplate);
+                });
             });
         }
     } else {
