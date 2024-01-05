11bf8be2049c15243400a6da51d3aecfe40463ec
sim <simyechan87@gmail.com>
2024-01-05 09:59:26 +0900

    함수

---
+++ syntax/function.js
@@ -0,0 +1,23 @@
+f123();
+console.log('A');
+console.log('B');
+console.log('C');
+f123();
+console.log('D');
+console.log('E');
+console.log('F');
+console.log('G');
+f123();
+console.log('H');
+console.log('I');
+console.log('J');
+console.log('K');
+console.log('L');
+f123();
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
