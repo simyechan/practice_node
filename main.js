62cc5524bc2c0e8c2fd01cd876e5efc76758da5a
sim <simyechan87@gmail.com>
2024-01-08 17:02:53 +0900

    mysql 상세보기 구현

--- main.js
+++ main.js
@@ -33,29 +33,54 @@ const app = http.createServer(function(request,response){
                 response.end(html);
             });
         } else {
-            fs.readdir('./data', function(error, filelist) {
-                var filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-                    var title = queryData.id;
-                    var sanitizedTitle = senitizeHtml(title);
-                    var sanitizedDescription = senitizeHtml(description, {
-                        allowedTags:['h1']
-                    });
-                    var list = template.list(filelist);
-                    var html = template.html(sanitizedTitle, list, 
-                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`, 
-                        `<a href="/create">create<a> <a href="/update?id=${sanitizedTitle}">update</a>
+            // fs.readdir('./data', function(error, filelist) {
+            //     var filteredId = path.parse(queryData.id).base;
+            //     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+            //         var title = queryData.id;
+            //         var sanitizedTitle = senitizeHtml(title);
+            //         var sanitizedDescription = senitizeHtml(description, {
+            //             allowedTags:['h1']
+            //         });
+            //         var list = template.list(filelist);
+            //         var html = template.html(sanitizedTitle, list, 
+            //             `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`, 
+            //             `<a href="/create">create<a> <a href="/update?id=${sanitizedTitle}">update</a>
+            //             <form action="delete_process" method="post">
+            //             <input type="hidden" name="id" value="${sanitizedTitle}">
+            //             <input type="submit" value="delete">
+            //             </form>`
+            //         );
+            //         response.writeHead(200);
+            //         response.end(html);
+            //     });
+            // });
+            db.query(`SELECT * FROM topic`, function(error, topics) {
+                if(error) {
+                    throw error;
+                }
+                db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic) {
+                    if(error2) {
+                        throw error2;
+                    }
+                    console.log(topic.title);
+                    var title = topic[0].title;
+                    var description = topic[0].description;
+                    var list = template.list(topics);
+                    var html = template.html(title, list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create<a>
+                        <a href="/update?id=${queryData.id}">update</a>
                         <form action="delete_process" method="post">
-                        <input type="hidden" name="id" value="${sanitizedTitle}">
-                        <input type="submit" value="delete">
+                            <input type="hidden" name="id" value="${queryData.id}">
+                            <input type="submit" value="delete">
                         </form>`
                     );
-                    response.writeHead(200);
-                    response.end(html);
-                });
+                response.writeHead(200);
+                response.end(html);
+                })
             });
         }
-    }else if(pathname === '/create') {
+    } else if(pathname === '/create') {
         fs.readdir('./data', function(error, filelist) {
             var title = 'WEB - create';
             var list = template.list(filelist);
