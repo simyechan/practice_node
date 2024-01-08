8d1f220dd7a0cd19fff38dcf1e2f71722db5555e
sim <simyechan87@gmail.com>
2024-01-08 09:44:03 +0900

    입출력에 대한 보안

--- main.js
+++ main.js
@@ -3,33 +3,44 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
+var senitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    console.log(pathname);
     if(pathname === '/') {
         if(queryData.id === undefined) {
             fs.readdir('./data', function(error, filelist) {
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-            var list = template.list(filelist);
-            var html = template.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create<a>`);
-            response.writeHead(200);
-            response.end(html);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = template.list(filelist);
+                var html = template.html(title, list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create<a>`
+                );
+                response.writeHead(200);
+                response.end(html);
             });
         } else {
             fs.readdir('./data', function(error, filelist) {
-                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var filteredId = path.parse(queryData.id).base;
+                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
                     var title = queryData.id;
+                    var sanitizedTitle = senitizeHtml(title);
+                    var sanitizedDescription = senitizeHtml(description, {
+                        allowedTags:['h1']
+                    });
                     var list = template.list(filelist);
-                    var html = template.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create<a> <a href="/update?id=${title}">update</a> 
-                    <form action="delete_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
+                    var html = template.html(sanitizedTitle, list, 
+                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`, 
+                        `<a href="/create">create<a> <a href="/update?id=${sanitizedTitle}">update</a>
+                        <form action="delete_process" method="post">
+                        <input type="hidden" name="id" value="${sanitizedTitle}">
                         <input type="submit" value="delete">
-                    </form>
-                    `);
+                        </form>`
+                    );
                     response.writeHead(200);
                     response.end(html);
                 });
@@ -69,7 +80,8 @@ var app = http.createServer(function(request,response){
         });
     } else if(pathname === '/update') {
         fs.readdir('./data', function(error, filelist) {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var filteredId = path.parse(queryData.id).base;
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
                 var title = queryData.id;
                 var list = template.list(filelist);
                 var html = template.html(title, list, 
@@ -114,7 +126,8 @@ var app = http.createServer(function(request,response){
         request.on('end', function() {
             var post = qs.parse(body);
             var id = post.id;
-            fs.unlink(`data/${id}`, function(error) {
+            var filteredId = path.parse(id).base;
+            fs.unlink(`data/${filteredId}`, function(error) {
                 response.writeHead(302, {location: `/`});
                 response.end();
             });
